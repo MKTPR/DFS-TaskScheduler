7931e0d8e1353cf2d13f59dcc5bb11d988a28dd7
dlee658 <55651714+dlee658@users.noreply.github.com>
2020-08-11 14:26:40 +1200

    delete

--- README.md
+++
@@ -1,3 +0,0 @@
-# project-1-team-7-project-1
-project-1-team-7-project-1 created by GitHub Classroom
-This is a Read.me File. We will need to work on this later
--- Task Scheduler/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    //Main class Empty
-    //test
-    public static void main(String[] args){
-        System.out.println("0652es342");
-    }
-
-}
