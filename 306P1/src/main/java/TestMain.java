e8cbc9367f468d946c5ef1c702075e877ae7b8ca
MKTPR <marickimtpr@gmail.com>
2020-08-11 17:04:15 +1200

    Testing for I/O from .dot file

--- 306P1/src/main/java/TestMain.java
+++ 306P1/src/main/java/TestMain.java
@@ -1,5 +1,38 @@
+import com.paypal.digraph.parser.GraphEdge;
+import com.paypal.digraph.parser.GraphNode;
+import com.paypal.digraph.parser.GraphParser;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
 public class TestMain {
-    public static void main(String[] args){
-        System.out.println("test123");
+
+    public static void main(String[] args) {
+        GraphParser parser = null;
+        try {
+            parser = new GraphParser(new FileInputStream("digraph.dot"));
+            Map<String, GraphNode> nodes = parser.getNodes();
+            Map<String, GraphEdge> edges = parser.getEdges();
+
+            log("--- nodes:");
+            for (GraphNode node : nodes.values()) {
+                log(node.getId() + " " + node.getAttributes());
+            }
+
+            log("--- edges:");
+            for (GraphEdge edge : edges.values()) {
+                log(edge.getNode1().getId() + "->" + edge.getNode2().getId() + " " + edge.getAttributes());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    private static void log(String s) {
+        System.out.println(s);
+    }
+
 }
+
