3c246352f22db9de99cab38b091d5619c2bca2ff
MKTPR <marickimtpr@gmail.com>
2020-08-11 14:14:45 +1200

    Deleting Duplicate Idea Folder

--- Task Scheduler/src/main/java/Main.java
+++ Task Scheduler/src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     //Main class Empty
     public static void main(String[] args){
-        System.out.println("0652342");
+        System.out.println("0652es342");
     }
 
 }
