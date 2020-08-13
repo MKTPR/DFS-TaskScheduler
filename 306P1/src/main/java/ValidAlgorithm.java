ef3db5eb091971afc16b27bbaaecdc14ead49778
dlee658 <55651714+dlee658@users.noreply.github.com>
2020-08-14 01:48:47 +1200

    add constuctor for valid algorithm class

--- 306P1/src/main/java/ValidAlgorithm.java
+++ 306P1/src/main/java/ValidAlgorithm.java
@@ -1,4 +1,14 @@
-public class ValidAlgorithm {
+import java.util.ArrayList;
 
+public class ValidAlgorithm {
+    private ArrayList<Node> _nodeList =new ArrayList<Node>();
+    private ArrayList<Edge> _edgeList =new ArrayList<Edge>();
+    private ArrayList<Processor> _processorList =new ArrayList<Processor>();
+
+    public ValidAlgorithm(ArrayList<Node> nodeList, ArrayList<Edge> edgeList, ArrayList<Processor> processorList) {
+        _nodeList = nodeList;
+        _edgeList = edgeList;
+        _processorList = processorList;
+    }
 }
 
