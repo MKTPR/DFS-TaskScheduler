449b101b5aaaea7b9e33f721f45d54b68995bcaa
dlee658 <55651714+dlee658@users.noreply.github.com>
2020-08-14 03:05:28 +1200

    implement scheduling algorithm

--- 306P1/src/main/java/ValidAlgorithm.java
+++ 306P1/src/main/java/ValidAlgorithm.java
@@ -16,9 +16,10 @@ public class ValidAlgorithm {
               ComputeFinishingTime(availableNode); //여기 !!!!
             if (availableNode.size() >= 2){
                 FindLargestEdge(availableNode);
-            } else{
-
             }
+
+            //availableNode.get(0)._process //맨왼쪽에 들어가있는 node
+                                            //Node class에 안에 있는 _process public으로 바꿔야함
             //Add selected n to corresponding Pi then remove the n from G
           }
 
@@ -38,8 +39,16 @@ public class ValidAlgorithm {
      * This method find the nodes that finish earliest
      */
     public void ComputeFinishingTime(ArrayList<Node> availableNode){
-        //for every nodes in availableNode{
-        //remove all nodes that are not finish earliest  }
+
+        //for every nodes (i) in availableNode, find a processor (j) that has shorstest finishing time and set processor
+        //availableNode.get(i).setProcessor("j"); i랑 j는 int
+
+        //for every nodes in availableNode remove all nodes that are not finish earliest
+
+        //여기서 node.setProcessor를 해도 괜찮음, 왜냐하면 while 루프 한번에 earlist finishing time인 node 하나씩 _nodeList에서 지워가는데
+        //_nodeList에서 지워지는 earlist finishing time인 node의 processor는 여기서 지정하는 processor에 들어가는 node이고
+        //earlist finishing time이 아니라서 이번 while 루프에서 안지워지는 node는 다음 while loop때 다시 setProcessor를 하기 때문에
+        //마지막 루프가 끝날때는 모든 node는 제대로된 processor에 들어가게됨
     }
 
     /**
