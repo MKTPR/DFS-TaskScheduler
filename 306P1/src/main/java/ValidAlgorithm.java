6f0b1bc7060d5962de7de3b69aee2be121fbb6f7
dlee658 <55651714+dlee658@users.noreply.github.com>
2020-08-14 15:40:22 +1200

    implement

--- 306P1/src/main/java/ValidAlgorithm.java
+++ 306P1/src/main/java/ValidAlgorithm.java
@@ -76,7 +76,7 @@ public class ValidAlgorithm {
         }
 
         //For each processor, try
-        
+        //node.setProcessor(_processorList.get(j).get_processorNumber());
 
 
 
@@ -109,18 +109,13 @@ public class ValidAlgorithm {
 
         // availableNodes 안에 있는 node들중에 processor에 넣으면 가장 적은 end time을 갖고 있는 노드들만 남기고 다 지운다
 
-
         //for every nodes (i) in availableNode, find a processor (j) that has shorstest finishing time and set processor
-        // availableNode.get(i).setProcessor(_processorList.get(j).get_processorNumber()); i랑 j는 int,
+
 
         //for every nodes in availableNode remove all nodes that are not finish earliest
 
 
-        //혹시 너가 의문을 품을까봐 적어놓음
-        //여기서 node.setProcessor를 해도 괜찮음, 왜냐하면 while 루프 한번에 earlist finishing time인 node 하나씩 _nodeList에서 지워가는데
-        //_nodeList에서 지워지는 earlist finishing time인 node의 processor는 여기서 지정하는 processor에 들어가는 node이고
-        //earlist finishing time이 아니라서 이번 while 루프에서 안지워지는 node는 다음 while loop때 다시 setProcessor를 하기 때문에
-        //마지막 루프가 끝날때는 모든 node는 제대로된 processor에 들어가게됨
+
 
         
     }
