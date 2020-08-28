7fee205e6595613bab7038bf9b45c67d78815a56
dlee658 <55651714+dlee658@users.noreply.github.com>
2020-08-28 16:47:16 +1200

    implement creategraph class

--- 306P1/src/main/java/Visualization/CreateGraph.java
+++ 306P1/src/main/java/Visualization/CreateGraph.java
@@ -5,7 +5,7 @@ import org.graphstream.graph.implementations.*;
 
 
 public class CreateGraph {
-
+    Graph _graph = new singleGraph("Graph");
     // Get list of all nodes
 
     // Get list of all edges
