51120efcf23159e307d14b1a8aaffbdd17598a18
dlee658 <55651714+dlee658@users.noreply.github.com>
2020-08-28 17:59:00 +1200

    implement

--- 306P1/src/main/java/Visualization/CreateGraph.java
+++ 306P1/src/main/java/Visualization/CreateGraph.java
@@ -3,13 +3,22 @@ package Visualization;
 import com.sun.org.apache.xml.internal.security.algorithms.JCEMapper;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
+ import java.util.ArrayList;
 
 public class CreateGraph {
     Graph _graph = new SingleGraph("Graph");
-    // Get list of all nodes
-    for (Algorithm.Node _node: Algorithm.TestMain.getNodeList()){
+    ArrayList<Algorithm.Node> _nodesList;
+    ArrayList<Algorithm.Edge> _edgesList;
 
+    CreateGraph(){
+        
     }
+    // Get list of all nodes
+    public void inputNode(){
+        for(Algorithm.Node _node:Algorithm.TestMain.getNodeList()){
+            _graph.addNode(_node.getName());
+        }
+}
     // Get list of all edges
 
 }
