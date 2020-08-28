d0f2074c5aaafec8fde6ce6f1e90ae1efdcacf65
dlee658 <55651714+dlee658@users.noreply.github.com>
2020-08-28 17:27:26 +1200

    test

--- 306P1/src/main/java/Visualization/CreateGraph.java
+++ 306P1/src/main/java/Visualization/CreateGraph.java
@@ -2,10 +2,9 @@ package Visualization;
 
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
-import Algorithm.Node;
 
 public class CreateGraph {
-    Graph _graph = new singleGraph("Graph");
+    Graph _graph = new SingleGraph("Graph");
     // Get list of all nodes
 
     // Get list of all edges
