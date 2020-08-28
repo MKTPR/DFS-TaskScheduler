e0644aba0e50577565a5e92975f67a381c63d17d
dlee658 <55651714+dlee658@users.noreply.github.com>
2020-08-28 18:16:59 +1200

    implement input for the graph

--- 306P1/src/main/java/Visualization/CreateGraph.java
+++ 306P1/src/main/java/Visualization/CreateGraph.java
@@ -9,9 +9,11 @@ public class CreateGraph {
     ArrayList<Algorithm.Node> _nodesList;
     ArrayList<Algorithm.Edge> _edgesList;
 
-    CreateGraph(ArrayList<Algorithm.Node> nodeList, ArrayList<Algorithm.Edge> edgelist){
+    CreateGraph(ArrayList<Algorithm.Node> nodeList, ArrayList<Algorithm.Edge> edgeList){
         _nodesList = nodeList;
-        _edgesList = edgelist;
+        _edgesList = edgeList;
+        parseInput();
+        _graph.display();
     }
 
     // Get list of all nodes
@@ -20,9 +22,10 @@ public class CreateGraph {
             _graph.addNode(_node.getName());
         }
         for(Algorithm.Edge _edge:_edgesList){
+            //string of each Node in edgeList
             _graph.addEdge(_edge.getStartNode().getName() + _edge.getEndNode().getName(),_edge.getStartNode().getName(),_edge.getEndNode().getName());
         }
-}
+    }
     // Get list of all edges
 
 }
