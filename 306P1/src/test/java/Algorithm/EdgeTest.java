f8f84d75bb85e4b1ba70529fc669540f3858f638
blakehattingh <blake.hattingh123@gmail.com>
2020-08-30 18:06:25 +1200

    Junit test for edge class - pair programming with Gurpreet (same with last few commits)

--- 306P1/src/test/java/Algorithm/EdgeTest.java
+++ 306P1/src/test/java/Algorithm/EdgeTest.java
@@ -8,8 +8,19 @@ import static org.junit.Assert.*;
 
 public class EdgeTest {
 
+    private Node _startNode;
+    private Node _endNode;
+    private int _weight;
+    private Edge _edge;
+
     @Before
     public void setUp() throws Exception {
+        _startNode = new Node("a");
+        _endNode = new Node("b");
+        _weight = 3;
+        _edge = new Edge(_startNode,_endNode);
+        _edge.setWeight("{weight=500}");
+
     }
 
     @After
@@ -18,17 +29,22 @@ public class EdgeTest {
 
     @Test
     public void getStartNode() {
+        assertEquals(_startNode, _edge.getStartNode());
     }
 
     @Test
     public void getEndNode() {
+        assertEquals(_endNode, _edge.getEndNode());
     }
 
     @Test
     public void getWeight() {
+        assertEquals(500, _edge.getWeight());
     }
 
     @Test
     public void testToString() {
+        String output = "a->b[Weight=500];";
+        assertEquals(output,_edge.toString().replace(" ", "").replace("\t",""));
     }
 }
